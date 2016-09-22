7d17e223a217787b5c82445533b2a46aef57c6e9
Lennon Jesus <lennon.jesus@gmail.com>
2016-09-22 17:24:58 -0300

    adding with and height opts

--- src/sparkline.directive.js
+++ src/sparkline.directive.js
@@ -2,34 +2,36 @@
   'use strict';
 
   angular.module('angular-sparkline-charts')
-    .directive('sparkline', function () {
+  .directive('sparkline', function () {
 
-      return {
-        restrict: 'E',
-        scope: {
-            data: "=",
-            options: "="
-        },
-        link: function (scope, elem, attrs, ngModel) {
+    return {
+      restrict: 'E',
+      scope: {
+        data: "=",
+        options: "="
+      },
+      link: function (scope, elem, attrs, ngModel) {
+        
+        var opts={};
 
-            var opts={};
+        opts.type = attrs.type || 'pie';
+        opts.width = attrs.width || 24;
+        opts.height = attrs.height || 24;
 
-            opts.type = attrs.type || 'line';
+        scope.$watch(scope.data, function () {
+          render();
+        });
 
-            scope.$watch(scope.data, function () {
-                render();
-            });
+        scope.$watch(attrs.opts, function(){
+          render();
+        });
 
-            scope.$watch(attrs.opts, function(){
-              render();
-            });
+        var render = function () {
+          $(elem).sparkline(scope.data, opts);
+        };
+      }
+    };
 
-            var render = function () {
-                $(elem).sparkline(scope.data, opts);
-            };
-        }
-      };
-
-    });
+  });
 
 })();
